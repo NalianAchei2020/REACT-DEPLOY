72351e76cc8f874091447d8f71a74a646a5251ee
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-12-27 18:20:00 +0100

    Update Navbar.js

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,23 +1,35 @@
+import { Navbar, Nav } from 'react-bootstrap';
 
-import {Navbar, Nav} from 'react-bootstrap';
-
-export const Header = ()=>{
-    return(
-       <>
-
-       <Navbar bg="whitesmoke" expand="sm" collapseOnSelect sticky='top' className='first'>
-           <Navbar.Brand className='second'></Navbar.Brand> 
-           <Navbar.Toggle/>
-           <Navbar.Collapse className='fouth'>
-           <Nav>
-               <Nav.Link href ="./" className='third'>Home</Nav.Link>
-               <Nav.Link href ="./works" className='third'>How it works</Nav.Link>
-               <Nav.Link href ="./Blog" className='third'>Blog</Nav.Link>
-               <Nav.Link href ="./SIGNIN" className='third'>Login</Nav.Link>
-           </Nav>
-</Navbar.Collapse>
-           </Navbar>
-
-       </>
-    );
-}
+const Header = () => {
+  return (
+    <>
+      <Navbar
+        bg="whitesmoke"
+        expand="sm"
+        collapseOnSelect
+        sticky="top"
+        className="first"
+      >
+        <Navbar.Brand className="second"></Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="fouth">
+          <Nav>
+            <Nav.Link href="./" className="third">
+              Home
+            </Nav.Link>
+            <Nav.Link href="./works" className="third">
+              How it works
+            </Nav.Link>
+            <Nav.Link href="./Blog" className="third">
+              Blog
+            </Nav.Link>
+            <Nav.Link href="./SIGNIN" className="third">
+              Login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+};
+export default Header;
