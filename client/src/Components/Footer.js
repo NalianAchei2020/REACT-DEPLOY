093865c9005e8250371a83782f35fbf4f1fc6d44
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-12-27 18:19:54 +0100

    Update Footer.js

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,33 +1,78 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-export const Footer = ()=>{
-    return(
-        <>
-        <div className="container-fluid bg-dark" id='footerCon' style={{color:"white"}}><br/><br/>
-            <center>
-        <div className="row">
+const Footer = () => {
+  return (
+    <>
+      <div
+        className="container-fluid bg-dark"
+        id="footerCon"
+        style={{ color: 'white' }}
+      >
+        <br />
+        <br />
+        <center>
+          <div className="row">
             <div className="col-sm-4">
-                <h6 className='footH'>ABOUT US</h6>
-                <hr/>
-                <p><Link to='/' style={{color:"white", textDecoration:"none"}}>Contact</Link></p>
-                <p><Link to='/'style={{color:"white", textDecoration:"none"}}>Career</Link></p>
+              <h6 className="footH">ABOUT US</h6>
+              <hr />
+              <p>
+                <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
+                  Contact
+                </Link>
+              </p>
+              <p>
+                <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
+                  Career
+                </Link>
+              </p>
             </div>
             <div className="col-sm-4">
-                <h6 className='footH'>RESOURCES</h6>
-                <hr/>
-                <p><Link to='/Help' style={{color:"white", textDecoration:"none"}}>Help Center</Link></p>
-                <p><Link to='/Blog' style={{color:"white", textDecoration:"none"}}>EasyPlan Blog</Link></p>
+              <h6 className="footH">RESOURCES</h6>
+              <hr />
+              <p>
+                <Link
+                  to="/Help"
+                  style={{ color: 'white', textDecoration: 'none' }}
+                >
+                  Help Center
+                </Link>
+              </p>
+              <p>
+                <Link
+                  to="/Blog"
+                  style={{ color: 'white', textDecoration: 'none' }}
+                >
+                  EasyPlan Blog
+                </Link>
+              </p>
             </div>
             <div className="col-sm-4">
-                <h6 className='footH'>SOCIAL MEDIA</h6>
-                <hr/>
-                <p><Link to='/www.instagramcom'style={{color:"white", textDecoration:"none"}}>Instagram</Link><br/></p>
-                <p><Link to='/facebook.com' style={{color:"white", textDecoration:"none"}}>Facebook</Link><br/></p>
+              <h6 className="footH">SOCIAL MEDIA</h6>
+              <hr />
+              <p>
+                <Link
+                  to="/www.instagramcom"
+                  style={{ color: 'white', textDecoration: 'none' }}
+                >
+                  Instagram
+                </Link>
+                <br />
+              </p>
+              <p>
+                <Link
+                  to="/facebook.com"
+                  style={{ color: 'white', textDecoration: 'none' }}
+                >
+                  Facebook
+                </Link>
+                <br />
+              </p>
             </div>
-        </div>
+          </div>
         </center>
-
-    </div>
+      </div>
     </>
-    )
-} 
+  );
+};
+
+export default Footer;
